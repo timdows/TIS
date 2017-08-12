ce5874c7fcf9db1df2544f7d699f1eeaa41e5780
Tim <tim@inexpro.nl>
2017-08-12 13:37:43 +0200

    Adding identity server project

--- TimdowsIdentityServer/Startup.cs
+++ TimdowsIdentityServer/Startup.cs
@@ -12,6 +12,8 @@ namespace TimdowsIdentityServer
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+			services.AddMvc();
+
 			services.AddIdentityServer()
 				.AddDeveloperSigningCredential()
 				.AddInMemoryApiResources(Config.GetApiResources())
@@ -32,10 +34,13 @@ namespace TimdowsIdentityServer
                 app.UseDeveloperExceptionPage();
             }
 
-            app.Run(async (context) =>
-            {
-                await context.Response.WriteAsync("Hello World!");
-            });
-        }
+			//app.Run(async (context) =>
+			//{
+			//    await context.Response.WriteAsync("Hello World!");
+			//});
+
+			app.UseStaticFiles();
+			app.UseMvcWithDefaultRoute();
+		}
     }
 }
